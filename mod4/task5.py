6a411bfcaca4631fe8b8d05c4c885ee32cffa7d8
BlessedDrained <92720096+BlessedDrained@users.noreply.github.com>
2023-03-18 21:49:50 +0500

    Some changes

--- mod4/task5.py
+++ mod4/task5.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 @app.route('/ps', methods=['GET'])
 def get_ps():
     args_raw = request.args.getlist('arg')
-    args = shlex.quote("".join(args_raw))
+    args = "".join([shlex.quote(x) for x in args_raw])
     command = f"ps {args}".split()
     run_result = subprocess.run(command, capture_output=True)
-    cmd_output = run_result.stdout
+
+    if run_result.returncode != 0:
+        return "Internal server error", 500
+
+    cmd_output = run_result.stdout.decode()
     return f"<pre>Your result: {cmd_output}</pre>"
 
 
