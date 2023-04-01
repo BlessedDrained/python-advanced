ec7a2f5c23027406eae880e55a2a4b2258de6f6c
BlessedDrained <92720096+BlessedDrained@users.noreply.github.com>
2023-04-01 19:55:30 +0500

    Fixed task3 in mod6

--- mod6/task3.py
+++ mod6/task3.py
@@ -1,29 +1,16 @@
 import json
-
-from flask import Flask
-from wtforms import StringField, IntegerField
-from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, Email
-from application.mod4.task2 import number_length, NumberLength
 import logging
+import getpass
+import hashlib
+from task2 import is_strong_password
 
 
 class JsonAdapter(logging.LoggerAdapter):
     def process(self, msg, kwargs):
+        msg = msg.replace('"', "'")
         msg = json.dumps(msg)
+        msg = msg.replace('"', "")
         return msg, kwargs
-
-
-class RegistrationForm(FlaskForm):
-    class Meta:
-        csrf = False
-
-    email = StringField(validators=[InputRequired(), Email()])
-    phone = IntegerField(validators=[InputRequired(), NumberLength(10, 10, 'Incorrect number length')])
-    name = StringField(validators=[InputRequired()])
-    address = StringField(validators=[InputRequired()])
-    index = IntegerField(validators=[InputRequired()])
-    comment = StringField(validators=[InputRequired()])
 
 
 logging.basicConfig(
@@ -34,20 +21,47 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+words = None
 logger = JsonAdapter(logging.getLogger('AuthLogger'), extra=None)
-app = Flask(__name__)
 
 
-@app.route('/registration', methods=['POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        email, phone = form.email.data, form.phone.data
-        logger.info(f"Successfully registered user {email} with phone +7{phone}")
-        return f"Successfully registered user {email} with phone +7{phone}"
-    logger.error(f"{form.errors}")
-    return f"{form.errors}", 400
+def input_and_check_password():
+    password = getpass.getpass('Enter the password: ')
+    if not password:
+        logger.warning('Input password is empty')
+        return False
+    if not is_strong_password(password, words):
+        logger.info('Password contains english word(s). Try another one')
+    try:
+        hasher = hashlib.md5()
+        hasher.update(password.encode("latin-1"))
+        if hasher.hexdigest() == "098f6bcd4621d373cade4e832627b4f6":
+            return True
+    except ValueError as exception:
+        logger.exception('There is incorrect symbol in password: ', exc_info=exception)
+    return False
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    with open('words.txt', 'r') as f:
+        words = set(sorted(x for x in f.read().splitlines() if len(x) > 4))
+    logging.basicConfig(
+        filename='stderr.txt',
+        filemode='a',
+        format='{"time": "%(asctime)s", "level": "%(levelname)s", "message": %(message)s}',
+        datefmt='%H:%M:%S',
+        level=logging.INFO
+    )
+    logger = logging.getLogger('AuthLogger')
+
+    attempts_count = 3
+    while attempts_count > 0:
+        logger.info(f'Attempts left: {attempts_count}')
+        if input_and_check_password():
+            logger.info('Successful auth')
+            exit(0)
+        logger.error('Incorrect password')
+        attempts_count -= 1
+
+    logger.error('No attempts left')
+    exit(1)
