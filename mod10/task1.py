e4a88057758588090827505bfff68ba92b5fdd11
BlessedDrained <92720096+BlessedDrained@users.noreply.github.com>
2023-04-01 22:43:59 +0500

    Removed redundant variable

--- mod10/task1.py
+++ mod10/task1.py
@@ -3,9 +3,8 @@ import sqlite3
 
 if __name__ == '__main__':
     pd.set_option('expand_frame_repr', False)
-    df = pd.read_csv('data.csv')
+    df = pd.read_csv('data1.csv')
     df.columns = ['id', 'number', 'car_name', 'description', 'owner']
 
     con = sqlite3.connect('table_car.db')
     df.to_sql('table_car', con, if_exists='replace', index=False, )
-    d = 10
