2e1e65c6b8f66a9c1706c74c642727b4c3c14b65
BlessedDrained <92720096+BlessedDrained@users.noreply.github.com>
2023-04-01 19:43:11 +0500

    Fixed task2 in mod6

--- mod6/task2.py
+++ mod6/task2.py
@@ -1,27 +1,13 @@
-import re
-
-
-def get_all_words(string):
-    substrings = []
-    for i in range(len(string)):
-        substr = ""
-        for j in range(i, len(string)):
-            substr += string[j]
-            if len(substr) > 4:
-                substrings.append(substr)
-    return substrings
-
-
 def is_strong_password(password: str, words: set):
     password = password.lower()
-    substrings = get_all_words(password)
-    for substring in substrings:
-        if substring in words:
+    for word in words:
+        if word in password:
             return False
     return True
 
 
 if __name__ == '__main__':
     with open('words.txt', 'r') as f:
-        words = set(sorted(x for x in f.read().splitlines() if len(x) >= 4))
-    print(is_strong_password('jtfwjkfl', words))
+        words = set(x for x in f.read().splitlines() if len(x) > 4)
+    password = 'jtfwjkfl'
+    print(is_strong_password(password, words))
